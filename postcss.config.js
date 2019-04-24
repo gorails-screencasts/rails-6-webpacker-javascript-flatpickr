9d471302c9e2d09eb79554366512f0279a8e1f05
Chris Oliver <excid3@gmail.com>
2019-04-24 12:20:20 -0500

    Add purgcss to minify Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
@@ -9,6 +9,17 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
 }
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment
